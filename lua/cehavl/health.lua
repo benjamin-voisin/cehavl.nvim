8d1b3a3883295da4363b4fb175137e4470ac11d0
Benjamin V <benjamin.voisin@ens-rennes.fr>
2023-11-15 11:57:51 +0100

    🩺 check the presence of an LSP for the :CehavlAll function

--- lua/cehavl/health.lua
+++ lua/cehavl/health.lua
@@ -1,14 +1,18 @@
 local M = {}
 local cehavl = require("cehavl")
 M.check = function()
-  vim.health.start("cehavl report")
-  -- make sure setup function parameters are ok
-  if cehavl.is_setup then
-    vim.health.ok("Setup is correct")
-  else
-    vim.health.error("Setup is incorrect")
-  end
-  -- do some more checking
-  -- ...
+    vim.health.start("cehavl report")
+    -- make sure setup function parameters are ok
+    if cehavl.is_setup then
+        vim.health.ok("Setup is correct")
+    else
+        vim.health.error("Setup is incorrect")
+    end
+
+    if vim.lsp.util.get_progress_messages()[1] then
+        vim.health.ok("LSP is loaded")
+    else
+        vim.health.warn("No LSP loaded, the function :CehavlAll will not work")
+    end
 end
 return M
